0178b427c05e1a7ba98ddd99e34bbea5ec770216
Gonz <78351336+gonzyui@users.noreply.github.com>
2021-03-29 20:53:49 +0200

    updated database managers and players data

--- src/me/gonzyui/gameapi/database/Rank.java
+++ src/me/gonzyui/gameapi/database/Rank.java
@@ -6,35 +6,57 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public enum Rank {
 
-    PLAYER(0, "§7Joueur"),
-    VIP(1, "§6VIP"),
-    HELPER(2, "§eHelper"),
-    MODERATOR(3, "§9Modérateur"),
-    ADMIN(10, "§CAdministrateur");
+    PLAYER(0, "§7§5Admin", "§7Joueur", "§7"),
+    VIP(1, "§6§4Admin","§6VIP", "§6VIP "),
+    HELPER(2, "§e§3Admin","§eHelper", "§eHelper "),
+    MODERATOR(3, "§9§2Admin", "§9Modérateur", "§9Modérateur "),
+    ADMIN(10, "§c§1Admin", "§CAdmin","§cAdmin ");
 
     private int power;
-    private String name;
-    public static HashMap<Integer, Rank> grade;
+    private String name, orderRank, displayName;
+    public static Map<Integer, Rank> ranks = new HashMap<>();
+    //public static HashMap<Integer, Rank> grade;
 
-    private Rank(int power, String name) {
+    private Rank(int power, String orderRank, String name, String displayName) {
         this.power = power;
         this.name = name;
+        this.orderRank = orderRank;
+        this.displayName = displayName;
     }
 
-    public static Rank getPowerRank(int power) {
-        return grade.get(power);
+    static {
+        for (Rank rank : Rank.values()) {
+            ranks.put(rank.getPower(), rank);
+        }
     }
+
+    /* static Rank getPowerRank(int power) {
+        return grade.get(power);
+    }*/
 
     public String getName() {
         return name;
     }
 
+    public static Rank powerToRank(int power) {
+        return ranks.get(power);
+    }
+
     public int getPower() {
         return power;
+    }
+
+    public String getOrderRank() {
+        return orderRank;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 
     public static int getPlayerRank(UUID uuid) {
